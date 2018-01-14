4d66108b8fd1b1bd369d8b3bce2a09f36447e7a4
Unknown <charliematina@gmail.com>
2018-01-14 15:56:42 +1300

    Added more responsive code + Details
    Made changes to the details section on the single projects gallery page. Added in more responsive code for tablet and small laptop viewports.

--- projects/ikoiko.php
+++ projects/ikoiko.php
@@ -1,13 +1,22 @@
 <div class="project-container" id="ikoiko">
-	<h2>BEST ROUTE</h2>
+	<h2>IKO IKO</h2>
 	<div class="project-details">
 		<ul>
-			<li>2014 - PRESENT </li>
-			<li>GRAPHIC DESIGN</li>
-			<li><a href="">WEBSITE</a></li>
+			<li>
+				<div class="project-detail-label">ROLE</div>
+				<div class="project-detail-info">Graphic Designer</div>
+			</li>
+			<li>
+				<div class="project-detail-label">YEAR</div>
+				<div class="project-detail-info">2014 - Present</div>
+			</li>
+			<li>
+				<div class="project-detail-label">WEBSITE</div>
+				<div class="project-detail-info"><a href="http://www.ikoiko.co.nz" target="_blank">ikoiko.co.nz</a></div>
+			</li>
 		</ul>
 	</div>
-	<p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, distinctio consequuntur magni voluptates commodi enim. Sunt ipsam porro veniam inventore dolores, recusandae ipsa velit unde alias consequatur ea ex. Dolorum.</p>
+	<p class="description">For this past 4 years I have been the In-House Graphic Designer for <a href="http://www.ikoiko.co.nz" target="_blank">Iko Iko</a>. I create material for both digital and print platforms, some examples of this are: Posters, greeting cards, website banners, social media graphics + <more class=""></more></p>
 	<div class="image-gallery">
 		<img src="images/ikoiko/ikoiko-01.jpg" alt="iko iko work">
 		<img data-src="images/ikoiko/ikoiko-02.jpg" src="" alt="iko iko work">
